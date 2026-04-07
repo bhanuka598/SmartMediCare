750552dede0ea05ece1eeb4ffaa39b7edb228769
Nethmi Hewasinghe <nethmihewasinghe02@gmail.com>
2026-04-07 16:47:45 +0530

    Remove telemedicine service from appointment branch

--- services/telemedicine-service/src/routes/telemedicineRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const telemedicineController = require("../controllers/telemedicineController");
-
-router.post("/session/create", telemedicineController.createSession);
-router.get("/session/:appointmentId", telemedicineController.getSessionByAppointmentId);
-router.patch("/session/:appointmentId/end", telemedicineController.endSession);
-
-module.exports = router;
